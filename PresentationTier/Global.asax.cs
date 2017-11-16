31cdc6fdc3f0dedd7ee767600b19ae3fbda703f9
gegMa3ai <zdv3223666@gmail.com>
2017-11-16 04:13:16 +0300

    [AW - 0.1] Added logging.

--- PresentationTier/Global.asax.cs
+++ PresentationTier/Global.asax.cs
@@ -13,6 +13,7 @@ namespace PresentationTier
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            Logger.InitLogger();
         }
     }
 }
