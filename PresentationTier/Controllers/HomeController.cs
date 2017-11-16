0833fa714faa7df3f3c4fb826482594611a519ba
gegMa3ai <zdv3223666@gmail.com>
2017-11-16 04:13:16 +0300

    [AW - 0.1] Added logging.

---
+++ PresentationTier/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using log4net;
+using log4net.Config;
+using log4net.Repository.Hierarchy;
+
+namespace PresentationTier.Controllers
+{
+
+    public class HomeController : Controller
+    {
+
+        public ActionResult Index()
+        {
+            Logger.Log.Debug("Debug message");
+            Logger.Log.Error("Error message");
+            Logger.Log.Info("Info message");
+
+            return View();
+        }
+    }
+}
---
+++ PresentationTier/Logger.cs
@@ -0,0 +1,24 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+
+namespace PresentationTier
+{
+    ///
+    /// Logger - shell for log4net logger
+    ///
+    public static class Logger
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Logger));
+
+        public static ILog Log
+        {
+            get { return log; }
+        }
+
+        public static void InitLogger()
+        {
+            XmlConfigurator.Configure();
+        }
+    }
+}
